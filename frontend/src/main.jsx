af1f786de510cd98bb1cb159d75d5c47b4be2401
Gowthamsenthil-22 <gowthamsenthil383@gmail.com>
2025-12-12 20:57:45 +0530

    Fix import statement for index.css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 import './styles/global.css';
-import "./styles/index.css";
-
+import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
